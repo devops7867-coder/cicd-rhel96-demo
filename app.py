aa7b285bb885b3dfc8b743c0e4f074d2d4bba3f6
devops7867-coder <devops7867@gmail.com>
2026-03-30 01:14:12 -0400

    Fix typo in home route response message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CI/CD Pipeline Working on RHEL 9.6!\ Its my 9th Attempt \ Finally Did it."
+    return "CI/CD Pipeline Working on RHEL 9.6!\ Its my 9th Attempt \ Finally Did it right."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
